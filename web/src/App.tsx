7cce23d5c6a9c05777c9a7bec9b67a9b7815d972
Ronny Acacio <ronnyacacio27@gmail.com>
2020-08-03 13:17:17 -0300

    feat: config routes with react router

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 
+import Routes from './routes';
 import GlobalStyles from './styles/global';
 
 function App() {
   return (
     <>
-      <h1>Hello World</h1>
+      <Routes />
       <GlobalStyles />
     </>
   );
