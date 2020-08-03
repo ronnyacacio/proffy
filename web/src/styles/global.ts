62c2c9b077f9167a2def2302edce23084e325ee2
Ronny Acacio <ronnyacacio27@gmail.com>
2020-08-03 09:30:34 -0300

    feat: add global styles and fonts

--- web/src/styles/global.ts
+++ web/src/styles/global.ts
@@ -1,6 +1,41 @@
 import { createGlobalStyle } from 'styled-components';
 
 export default createGlobalStyle`
+* {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  html, body, #root {
+    height: 100vh;
+  }
+
+  body {
+    background: var(--background);
+  }
+
+  #root {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+
+  body, input, button, textarea {
+    font: 500 1.6rem Poppins;
+  }
+
+  .container {
+    width: 90vw;
+    max-width: 700px;
+  }
+
+  @media(min-width: 700px) {
+    :root {
+      font-size: 62.5%;
+    }
+  }
+
   :root {
     --background: #f0f0f7;
     --primary-lighter: #9871f5;
