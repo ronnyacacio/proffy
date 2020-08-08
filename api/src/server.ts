fdcfa4635ee570763301be7efd304d357d7f3ab1
Ronny Acacio <ronnyacacio27@gmail.com>
2020-08-08 14:07:29 -0300

    feat: add cors

--- api/src/server.ts
+++ api/src/server.ts
@@ -1,9 +1,11 @@
 import express, { json } from 'express';
+import cors from 'cors';
 
 import { routes } from './routes';
 
 const app = express();
 
+app.use(cors());
 app.use(json());
 app.use(routes);
 
