fe8b937b9d8d30b84ed8324a8a40bd34c8e475ca
Ronny Acacio <ronnyacacio27@gmail.com>
2020-08-08 09:45:00 -0300

    feat: create component PageHeader

--- web/src/routes/index.tsx
+++ web/src/routes/index.tsx
@@ -2,12 +2,16 @@ import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
 import Landing from '../pages/Landing';
+import TeacherList from '../pages/TeacherList';
+import TeacherForm from '../pages/TeacherForm';
 
 const Routes: React.FC = () => {
   return (
     <BrowserRouter>
       <Switch>
         <Route exact path="/" component={Landing} />
+        <Route path="/study" component={TeacherList} />
+        <Route path="/give-classes" component={TeacherForm} />
       </Switch>
     </BrowserRouter>
   );
