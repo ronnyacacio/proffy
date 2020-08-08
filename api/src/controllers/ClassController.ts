27b15a94e44ef1e11849260aae2ab0a307e3c971
Ronny Acacio <ronnyacacio27@gmail.com>
2020-08-08 13:50:39 -0300

    feat: add list classes

--- api/src/controllers/ClassController.ts
+++ api/src/controllers/ClassController.ts
@@ -10,6 +10,36 @@ interface IScheduleItem {
 }
 
 class ClassController {
+  async index(req: Request, res: Response) {
+    const filters = req.query;
+
+    const week_day = filters.week_day as string;
+    const subject = filters.subject as string;
+    const time = filters.time as string;
+
+    if (!week_day || !subject || !time)
+      return res
+        .status(400)
+        .json({ error: 'Missing filters to search classes' });
+
+    const timeInMinutes = convertHourToMinutes(time);
+
+    const classes = await db('classes')
+      .whereExists(function () {
+        this.select('class_schedule.*')
+          .from('class_schedule')
+          .whereRaw('`class_schedule`.`class_id` = `classes`.`id`')
+          .whereRaw('`class_schedule`.`week_day` = ??', [Number(week_day)])
+          .whereRaw('`class_schedule`.`from` <= ??', [timeInMinutes])
+          .whereRaw('`class_schedule`.`to` > ??', [timeInMinutes]);
+      })
+      .where('classes.subject', '=', subject)
+      .join('users', 'classes.user_id', '=', 'users.id')
+      .select(['classes.*', 'users.*']);
+
+    return res.json(classes);
+  }
+
   async store(req: Request, res: Response) {
     const { name, avatar, whatsapp, bio, subject, cost, schedule } = req.body;
 
--- api/src/routes.ts
+++ api/src/routes.ts
@@ -5,5 +5,6 @@ import ClassController from './controllers/ClassController';
 const routes = express.Router();
 
 routes.post('/classes', ClassController.store);
+routes.get('/classes', ClassController.index);
 
 export { routes };
